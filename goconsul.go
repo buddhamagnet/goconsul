74fa979699b9b5c7e05cc13231213fcc10f3dc4a
buddhamagnet <buddhamagnet@gmail.com>
2015-07-20 19:35:13 +0100

    extract funcs and make registration a function

--- goconsul.go
+++ goconsul.go
@@ -1,20 +1,15 @@
 package goconsul
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 )
 
 var (
-	endpointRegister string
-	endpointQuery    string
-	client           *http.Client
-	port             string
+	consul Consul
+	port   string
 )
 
 type Consul struct {
@@ -26,49 +21,46 @@ func init() {
 	if port == "" {
 		port = "8500"
 	}
-	registerService()
 }
 
-// NewClient returns a client struct with
-// a preset timeout.
-func NewClient() *http.Client {
-	return &http.Client{}
-}
-
-// registerService registers the service outlined in goconsul.json
+// RegisterService registers the service outlined in goconsul.json
 // with the local consul agent.
-func registerService() {
-	var consul Consul
+func RegisterService() (err error) {
+	// Open goconsul configuration file.
 	config, err := os.Open("goconsul.json")
-
-	if err != nil {
-		log.Fatal(err)
-	}
 	defer config.Close()
 
+	if err != nil {
+		return err
+	}
+
+	// Read configuration file for storage in struct and
+	// forward transmission to consul agent over HTTP API.
 	data, err := ioutil.ReadAll(config)
-
 	if err != nil {
-		log.Fatal(err)
+		return err
+	}
+	// Marshal response into consul struct for easy
+	// retrieval of service name for command line
+	// confirmations.
+	err = json.Unmarshal(data, &consul)
+	if err != nil {
+		return err
 	}
 
-	_ = json.Unmarshal(data, &consul)
-
-	endpointRegister = fmt.Sprintf("http://localhost:%s/v1/agent/service/register", port)
-	r, err := http.NewRequest("PUT", endpointRegister, bytes.NewBufferString(string(data)))
-
-	log.Printf("sending service registration request for %s\n", consul.Name)
-
-	client := NewClient()
-	_, err = client.Do(r)
-
+	// Register the service with local consul agent.
+	err = doRegistration(data)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	endpointQuery = fmt.Sprintf("http://localhost:%s/v1/catalog/service/%s", port, consul.Name)
-	r, err = http.NewRequest("GET", endpointQuery, nil)
-	resp, err := client.Do(r)
+	// Finally, query the API to confirm this has been done.
+	resp, err := queryService()
+
+	if err != nil {
+		return err
+	}
+
 	if resp.StatusCode == 200 {
 		response, _ := ioutil.ReadAll(resp.Body)
 		log.Println("service registration complete, please check response:")
@@ -76,4 +68,5 @@ func registerService() {
 	} else {
 		log.Fatalf("service registration failed: %d\n", resp.StatusCode)
 	}
+	return nil
 }
